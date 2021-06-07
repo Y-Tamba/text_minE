b88fb563f1380feafee9ae09aafd4512d9f16ce8
Y-Tamba <tambayvonne@gmail.com>
2021-06-07 18:00:51 +0300

    term frequency

--- SOC_in_NDCs.R
+++ SOC_in_NDCs.R
@@ -5,16 +5,31 @@ install.packages("pdftools")
 
 
 ##create a .txt dataset from pdf file using cabo verde to illustrate
-load("pdftools")
 text<-"Cabo_verde_INDC_.pdf"
 text<-pdftools::pdf_text(text) #extract text from pdf
 length(text)  ##check length of file
 text_dt<-write.table(text,file=paste("Cabo_verde_INDC",sep="."), 
  quote = FALSE, row.names = FALSE, col.names = FALSE, eol = " " )
 
+caboVerde_NDC<-textmineR::CreateDtm(doc_vec=text, 
+                              doc_names = NULL,
+                              ngram_window = c(1,1), # minimum and maximum n-gram length
+                              stopword_vec = c(stopwords::stopwords("en"), # stopwords from tm
+                                               stopwords::stopwords(source = "smart")), # this is the default value
+                              lower = TRUE, # lowercase - this is the default value
+                              remove_punctuation = TRUE, # punctuation - this is the default
+                              remove_numbers = TRUE, # numbers - this is the default
+                              verbose = TRUE # Turn off status bar for this demo
+) 
+
+caboVerde_NDC@Dimnames[[2]]
+wordcount<-textmineR::TermDocFreq(caboVerde_NDC)
+
 ##create a dtm matrix of all documents. Start with .csv input containing links to all documents
 docs_input<-(read.csv("NDCs_INDCs_Africa.csv"))
 docs_col<-docs_input[,4]
+text_dt<-write.table(text,file=paste("Cabo_verde_INDC",sep="."), 
+                     quote = FALSE, row.names = FALSE, col.names = FALSE, eol = " " )
 text_dt<-textmineR::CreateDtm(doc_vec=docs_col,
                               doc_names = NULL,
                      ngram_window = c(1,1), # minimum and maximum n-gram length
