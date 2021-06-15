e5bb64a37da28ae9d835b38286f1b4fe97374821
Y-Tamba <tambayvonne@gmail.com>
2021-06-15 16:35:12 +0300

    intro RSelenium for sraping dynamic links [error in xpath]

--- SOC_in_NDCs.R
+++ SOC_in_NDCs.R
@@ -5,8 +5,9 @@ install.packages("pdftools")
 install.packages("dplyr")
 library("dplyr")
 library("rvest")
-library(V8)
 library("xml2")
+library('RSelenium')
+
 ##create a .txt dataset from pdf file using cabo verde to illustrate
 text<-("Cabo_Verde_INDC_.pdf")
 
@@ -22,17 +23,66 @@ text_script3<-text_input %>%
   xml_attrs(xml_child(xml_child(xml_child(text_input2, 2), 1), 1))[["src"]] %>% 
   html_text()
 
-text<-xml_attrs(xml_child(xml_child(xml_child(xml_child(xml_child(xml_child(xml_child(text_input, 2), 2), 2), 1), 2), 6), 1))
 
 ##css selector all pages 
 text_input2<-rvest::read_html("https://www4.unfccc.int/sites/NDCStaging/Pages/All.aspx")
+table<-html_nodes(text_input2) 
 
+nodes<-text_input2 %>% 
+       html_node(id="divctl00_m_g_e7b2d30c_d41f_4a0c_9bd4_a7d111a5d6cb_ctl00")
+
+list<-for(i in nodes){
+  r <- list(nodes%>% html_nodes(" ")%>%html_text())
+}     
+
+nodes<-text_input2 %>% 
+  html_structure() 
+ 
+page <- read_html('https://finance.yahoo.com/quote/AAPL/financials?p=AAPL')
+nodes <- page %>%html_nodes(".fi-row")
+df = NULL
+
+for(i in nodes){
+  r <- list(i %>%html_nodes("[title],[data-test='fin-col']")%>%html_text())
+  df <- rbind(df,as.data.frame(matrix(r[[1]], ncol = length(r[[1]]), byrow = TRUE), stringsAsFactors = FALSE))
+}
+
+
+## selenium
+
+remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "chrome")
+
+remDr$open()
+url<-remDr$navigate("https://www4.unfccc.int/sites/NDCStaging/Pages/All.aspx")
+webpage <- read_html(remDr$getPageSource()[[1]])
+
+css_path2<-"html > body > form > div:nth-of-type(7) > div:nth-of-type(2) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(1) > table:nth-of-type(2) > tbody > tr:nth-of-type(1) > td > table > tbody > tr > td > div > div > div > div > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(2) > div > section:nth-of-type(1) > div > div > div > div:nth-of-type(3) > div:nth-of-type(2) > a"
+
+section<-remDr$findElement(using = "class",value= "col-sm-5")
+ 
+nodes<-webpage %>% 
+     html_node(class="col-sm-5") %>%  
+  html_attr(name = "href")
+   
+script<-html_node(webpage, xpath= "//table[contains(@href")#//class[contains(@href,',pdf')]")
+
+target_script<-html_node(webpage, xpath= "//form/*[@id='s4-workspace']") #/a[contains@href]') #,".pdf"]')
+
+path_1<-html_node(webpage,xpath= "//div[contains(@title, 'https://www4.unfccc.int/sites/ndcstaging/PublishedDocuments/Afghanistan%20First/INDC_AFG_20150927_FINAL.pdf')]")
+
+
+'https://www4.unfccc.int/sites/ndcstaging/PublishedDocuments/Afghanistan%20First ...
 text_script<-text_input2 %>%
-  html_node ("col-sm-2") %>% 
+  html_node (%>% ) %>%  
   html_text ()
 
+trypaths<-
+  
+ x_attribute_path<-xml_attrs(xml_child(xml_child(xml_child(xml_child(xml_child(xml_child(xml_child
+                  (xml_child(table[[1]], 29), 1), 1), 2), 2), 1), 3), 1))#["href"]
+
 text_script2<-text_input2 %>%
- xml_attrs(xml_child(xml_child(text_input2, 2), 4))[["href"]] %>% 
+ html_nodes(xpath=x_attribute_path) %>% 
   html_text()
 
 text_script3<-text_input2 %>%
